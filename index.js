ab8a1ea7105c6f2600554be3b509682415a1ec51
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-01 23:28:42 -0500

    npm inquirer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+console.log(inquirer)
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
