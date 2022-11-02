b2d41431deff49923180af0fc1d434abc30e659b
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-02 18:13:45 -0500

    questions

--- index.js
+++ index.js
@@ -4,7 +4,48 @@ console.log(inquirer)
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },{
+        type: "input",
+        message: "What was your motivation for this project?",
+        name: "motivation",
+    },{
+        type: "input",
+        message: "Why did you build this project?",
+        name: "whyBuild",
+    },{
+        type: "input",
+        message: "What problem does it solve?",
+        name: "problem",
+    },{
+        type: "input",
+        message: "What did you learn?",
+        name: "learn",
+    },{
+        type: "input",
+        message: "Describe the steps required to install the project.",
+        name: "install",
+    },{
+        type: "input",
+        message: "Provide instructions and examples of use.",
+        name: "usage",
+    },{
+        type: "input",
+        message: "Provide a screenshot of the project being used.",
+        name: "screenshot",
+    },{
+        type: "list",
+        message: "Choose a license for the application",
+        name: "license",
+        choices: ['MIT', 'Apache', 'Unlicense', 'GNU General Public License', 'Mozilla Public License', 'Eclipse Public License', 'Berkeley Source Distribution (BSD)'],
+    }
+        
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
